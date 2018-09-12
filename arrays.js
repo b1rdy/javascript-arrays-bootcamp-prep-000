55fcc452c73b4f34b53cc4d605fd43a9e470143d
Dorothy Davenport <dorothympdx@gmail.com>
2018-09-12 03:36:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,7 @@ function removeElementFromBeginningOfArray(array){
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
 }
+
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length - 1);
+}
